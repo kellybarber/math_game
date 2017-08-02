6b11a8ae4f203d5a6f4a0b0b121e36e5671e046d
kellybarber <kelly.james.barber@gmail.com>
2017-08-02 00:14:33 +0000

    working code, bit of a mess in places, but it uses three classes and gets the job done

--- test.rb
+++ test.rb
@@ -16,15 +16,28 @@ class Game
     @player2 = player2
   end
 
-  def minus_life
-    @player1.lives -= 1
-  end
-
   def game_loop(turn)
-    while @player1.lives > 0 || @player2.lives > 0
+    while @player1.lives > 0 && @player2.lives > 0
       turn.question(@player1)
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+      puts "----- NEW TURN -----"
+      if @player1.lives > 0
       turn.question(@player2)
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+      puts "----- NEW TURN -----"
+      end
     end
+
+    if @player1.lives === 0
+      puts "#{@player2.name} wins with a score of #{@player2.lives}/3"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+    else
+      puts "#{@player1.name} wins with a score of #{@player1.lives}/3"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+    end
+
   end
 
 end
@@ -51,7 +64,6 @@ class Turn
     end
 
   end
-
 end
 
 
